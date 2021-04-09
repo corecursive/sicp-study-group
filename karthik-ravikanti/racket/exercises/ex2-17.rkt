b3b4d6ffca2153801d10b8dd9dc94bb20b13be2e
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-04-09 21:23:24 +0800

    Fix typo

--- karthik-ravikanti/racket/exercises/ex2-17.rkt
+++ karthik-ravikanti/racket/exercises/ex2-17.rkt
@@ -1,4 +1,4 @@
-lang sicp
+#lang sicp
 
 ;; *Exercise 2.17:* Define a procedure `last-pair' that returns the
 ;; list that contains only the last element of a given (nonempty)
--- karthik-ravikanti/racket/exercises/ex2-18.rkt
+++ karthik-ravikanti/racket/exercises/ex2-18.rkt
@@ -1,4 +1,4 @@
-lang sicp
+#lang sicp
 
 ;; *Exercise 2.18:* Define a procedure `reverse' that takes a list as
 ;; argument and returns a list of the same elements in reverse order:
