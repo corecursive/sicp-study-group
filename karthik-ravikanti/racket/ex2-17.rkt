6a28bcc9da9ad91598fd0b3ee1962a88bfe935b1
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-04-09 21:00:09 +0800

    Solve ex2-17

---
+++ karthik-ravikanti/racket/ex2-17.rkt
@@ -0,0 +1,13 @@
+lang sicp
+
+;; *Exercise 2.17:* Define a procedure `last-pair' that returns the
+;; list that contains only the last element of a given (nonempty)
+;; list:
+
+(define (last-pair elems)
+  (define the-rest (cdr elems))
+  (if (null? the-rest)
+      (car elems)
+      (last-pair the-rest)))
+
+(last-pair (list 23 72 149 34))         ; (34)
